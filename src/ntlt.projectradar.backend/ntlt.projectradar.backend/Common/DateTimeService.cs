3b55cf65cbfaba6d43ff2175c96404f45827ab92
Tobias Waggoner <tobias.waggoner@gmail.com>
2025-06-11 12:51:53 +0200

    feat: added some abstractions for testing

---
+++ src/ntlt.projectradar.backend/ntlt.projectradar.backend/Common/DateTimeService.cs
@@ -0,0 +1,13 @@
+﻿namespace ntlt.projectradar.backend.Common;
+
+public interface IDateTimeService
+{
+    DateTime Now { get; }
+    DateTimeOffset NowOffset { get; }
+}
+
+public class DateTimeService : IDateTimeService
+{
+    public DateTime Now => DateTime.Now;
+    public DateTimeOffset NowOffset => DateTimeOffset.Now;
+}
---
+++ src/ntlt.projectradar.backend/ntlt.projectradar.backend/Common/DelayService.cs
@@ -0,0 +1,15 @@
+﻿namespace ntlt.projectradar.backend.Common;
+
+public interface IDelayService
+{
+    Task Delay(TimeSpan delay);
+}
+
+public class DelayService : IDelayService
+{
+    public async Task Delay(TimeSpan delay)
+    {
+        await Task.Delay(delay);
+    }
+    
+}
